3e7cbf990b036d11a9b9fd21f037c73e770dcde0
Jacob Fisher <jacobfisher18@gmail.com>
2018-07-19 22:00:52 -0700

    added artist endpoint

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('API is working!'));
+
+let artistData = [
+  {
+    "id": 111,
+    "name": "Tom Misch"
+  },
+  {
+    "id": 222,
+    "name": "Adele"
+  },
+  {
+    "id": 333,
+    "name": "Katy Perry"
+  }
+]
+
+app.get('/artists/:artistID/', (req, res) => {
+
+  let targetArtist = artistData.find( (artist) => {
+    return artist.id == req.params.artistID;
+  });
+
+  if (typeof targetArtist === "undefined") {
+    res.sendStatus(404);
+  } else {
+    res.send(targetArtist);
+  }
+});
+
+app.get('/world', (req, res) => res.send('World!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
